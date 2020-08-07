5c52cdc5cb5f05e09efb27dd1a7a4aae5412a7a8
Wan-Ting Hsieh <cv47522@gmail.com>
2020-08-07 18:17:45 +0300

    Add Main Menu

---
+++ Assets/Projects/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Feather");
+    }
+
+    public GameObject Panel;
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+    }
+
+}
